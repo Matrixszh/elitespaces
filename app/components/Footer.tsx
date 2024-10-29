fa715f03e7eb4afb698e07eded6c94e2f756ff64
Zain <zainhussaini9898@gmail.com>
2024-10-30 04:17:38 +0530

    fixed footer and slider

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,29 +2,29 @@ import Image from "next/image";
 import React from "react";
 import logo from "@/public/images/logo.jpeg";
 import { Instagram } from "lucide-react";
-import { FaFacebook} from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <div className="flex flex-col gap-20 justify-center items-center">
       <div>
         <div className="lg:flex lg:flex-row lg:gap-[40px] lg:w-full lg:px-[100px] py-20 font-Jakarta sm:flex sm:flex-col sm:text-center sm:gap-0 sm:px-8 md:w-full ">
-          <div className="lg:flex lg:items-center  ">
+          <div className="lg:flex lg:items-center">
             <p className="text-[34px] font-extrabold tracking-tighter text-center md:text-[40px] lg:text-[84px] lg:leading-[84px] lg:text-left md:text-center">
               Let&apos;s <br className="hidden" />{" "}
               <span className="text-[#D72323]">Connect</span>
             </p>
           </div>
-          <div className="bg-gray-400 w" />
-          <div className="lg:flex w-fit sm:flex sm:flex-col md:flex md:flex-row md:mt-4 ">
-            <div className="flex flex-col p-10 items-center w-fit max-sm:p-4">
-              <p className=" lg:text-3xl font-Jost">Alex</p>
+          <div className="bg-gray-400" />
+          <div className="lg:flex w-fit sm:flex sm:flex-col md:flex md:flex-row md:mt-4">
+            <div className="flex flex-col p-10 items-center w-fit max-sm:p-10 max-sm:items-center max-sm:justify-center">
+              <p className="lg:text-3xl font-Jost">Alex</p>
               <div className="flex flex-col mt-[38px] font-Jost text-center gap-1 text-black text-opacity-75 max-sm:text-[15px]">
                 <p>+1-630-709-1629</p>
                 <p>aalexelite@gmail.com</p>
               </div>
               <div className="mt-5 flex flex-col gap-1 text-black text-opacity-75 max-sm:text-[15px]">
-                <p className="text-center font-Jost ">
+                <p className="text-center font-Jost">
                   Chicago,
                   <br className="sm:hidden lg:block" /> USA
                 </p>
